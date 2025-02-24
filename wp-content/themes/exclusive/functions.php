ab1c3bdabcae1a71e170211217baaa79a5d788cd
vinhpt99 <vinhpt72@gmail.com>
2025-02-24 22:34:44 +0700

    apply font

--- wp-content/themes/exclusive/functions.php
+++ wp-content/themes/exclusive/functions.php
@@ -1,7 +1,13 @@
 <?php
-  function exclusive_theme_enqueue_styles() {
-    wp_enqueue_style('main-style', get_stylesheet_uri());
-    wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/exclusive.css', array(), '1.0.0', 'all');
+function exclusive_theme_enqueue_styles()
+{
+  wp_enqueue_style('main-style', get_stylesheet_uri());
+  wp_enqueue_style('exclusive-style', get_template_directory_uri() . '/assets/css/exclusive.css', array(), '1.0.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'exclusive_theme_enqueue_styles');
-?>
+
+function load_fontawesome()
+{
+  wp_enqueue_style('fontawesome', get_template_directory_uri() . '/assets/fonts/fontawesome/css/all.min.css', array(), '6.0.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'load_fontawesome');
