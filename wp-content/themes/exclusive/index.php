10575c60d6db26bd7e32418cff6e860bf5402b34
vinhpt99 <vinhpt72@gmail.com>
2025-02-24 22:42:12 +0700

    apply css hero section

--- wp-content/themes/exclusive/index.php
+++ wp-content/themes/exclusive/index.php
@@ -1,1 +1,39 @@
 <?php get_header(); ?>
+<section class="hero-section">
+  <div class="categories">
+    <ul id="treeMenu">
+      <li>Woman's Fashion <img src="../assets/icons/Vector.png" />
+        <ul>
+          <li>Subitem 1.1</li>
+          <li>Subitem 1.2</li>
+        </ul>
+      </li>
+      <li>Men's Fashion <img src="../assets/icons/Vector.png" />
+        <ul>
+          <li>Subitem 2.1</li>
+          <li>Subitem 2.2</li>
+        </ul>
+      </li>
+      <li>Electronics</li>
+      <li>Home & Lifestyle</li>
+      <li>Medicine</li>
+      <li>Sports & Outdoor</li>
+      <li>Baby's & Toys</li>
+      <li>Groceries & Pets</li>
+      <li>Health & Beauty</li>
+    </ul>
+  </div>
+  <div class="slideshow">
+    <div class="slides">
+      <div class="slide">
+        <img src="../assets/images/slide.png" alt="Slide 1">
+      </div>
+      <div class="slide">
+        <img src="../assets/images/slide.png" alt="Slide 2">
+      </div>
+      <div class="slide">
+        <img src="../assets/images/slide.png" alt="Slide 3">
+      </div>
+    </div>
+  </div>
+</section>
